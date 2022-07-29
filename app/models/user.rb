5fda381622f107c31626018411a6254743445702
kehan <ke_han@icloud.com>
2022-07-29 16:05:26 +0800

    User数据校验

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: /@/, uniqueness: true
+    validates :password, length: 6..20, on:[:create]
+    validates :password_confirmation, presence: true
 end
