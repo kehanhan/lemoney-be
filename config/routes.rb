d176cb239fe98f200694e042b07d702b647add15
kehan <ke_han@icloud.com>
2022-08-05 10:41:02 +0800

    创建 record 路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/users/me', to: 'users#me'
+      delete '/records/:id', to:'records#destroy'
       resource :users
       resource :sessions, only: [:create, :destroy]
+      resource :records
     end
   end
 end
